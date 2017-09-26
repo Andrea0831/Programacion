3f08b9089b552d581ab440ff7ae920948b7e8396
Melissa Andrea Avila <maavilao13@gamil.com>
2017-09-25 22:16:36 -0500

    Principal

---
+++ src/mi/clinica/MiClinica.java
@@ -0,0 +1,14 @@
+
+
+package mi.clinica;
+
+public class MiClinica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
